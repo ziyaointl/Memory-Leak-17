f57c08f2485a93a7fd20103d88703a426fff7020
Ziyao Zhang <ziyaointl@gmail.com>
2017-03-24 21:42:30 +0800

    Place everything in a hero

--- conf-website/src/App.vue
+++ conf-website/src/App.vue
@@ -1,35 +1,43 @@
 <template>
   <div id="app">
-    <div class="container">
-    <nav class="nav">
-      <div class="nav-left">
-        <a class="nav-item">
-          <h1 id="small-logo">I/O Exception</h1>
-        </a>
+
+    <section class="hero is-medium">
+      <div class="hero-head">
+        <nav class="nav">
+          <div class="nav-left">
+            <a class="nav-item">
+              <h1 id="small-logo">I/O Exception</h1>
+            </a>
+          </div>
+
+          <!--Toggle button, only visible on mobile-->
+          <span class="nav-toggle" @click="toggleMenu" v-bind:class="{ 'is-active': menuActive }">
+          <span></span>
+          <span></span>
+          <span></span>
+          </span>
+
+          <!-- nav-menu is hidden on mobile -->
+          <div class="nav-right nav-menu" v-bind:class="{ 'is-active': menuActive }">
+            <a class="nav-item">Home</a>
+            <a class="nav-item">Schedule</a>
+            <a class="nav-item">About</a>
+          </div>
+        </nav>
       </div>
 
-      <!--Toggle button, only visible on mobile-->
-      <span class="nav-toggle" @click="toggleMenu" v-bind:class="{ 'is-active': menuActive }">
-        <span></span>
-        <span></span>
-        <span></span>
-      </span>
 
-      <!-- nav-menu is hidden on mobile -->
-      <div class="nav-right nav-menu" v-bind:class="{ 'is-active': menuActive }">
-        <a class="nav-item">Home</a>
-        <a class="nav-item">Schedule</a>
-        <a class="nav-item">About</a>
+      <div class="hero-body">
+        <div class="container has-text-centered">
+          <h1 id="large-logo"><span id="io">I/O</span> Exception <span id="year">17</span></h1>
+          <h2 class="subtitle">Shandong Jinan</h2>
+          <h2 class="subtitle">August 9, 2017</h2>
+        </div>
       </div>
-    </nav>
-    </div>
+
+    </section>
 
 
-    <div class="container has-text-centered">
-      <h1 id="large-logo"><span id="io">I/O</span> Exception <span id="year">17</span></h1>
-      <h2 class="subtitle">Shandong Jinan</h2>
-      <h2 class="subtitle">August 9, 2017</h2>
-    </div>
   </div>
 </template>
 
